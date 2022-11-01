c352407992d6d26052b4587df3fe06692ccd9598
Bart Schilperoort <b.schilperoort@gmail.com>
2022-11-01 10:07:51 +0100

    First commit of cookiecutter template.

---
+++ pywflow/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about pywflow"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Bart Schilperoort"
+__email__ = "b.schilperoort@esciencecenter.nl"
+__version__ = "0.1.0"
