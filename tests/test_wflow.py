5cb6b224fad1558b1441b37ca534a6b0e93f658f
Bart Schilperoort <b.schilperoort@gmail.com>
2022-11-04 08:59:55 +0100

    Added basic pytest

---
+++ tests/test_wflow.py
@@ -0,0 +1,28 @@
+import pytest
+from pytest_print import printer
+from pywflow import WflowBMI
+import urllib
+import shutil
+
+
+staticmaps_src = "https://github.com/visr/wflow-artifacts/releases/download/v0.2.1/staticmaps-lahn.nc"
+forcing_src = "https://github.com/visr/wflow-artifacts/releases/download/v0.2.0/forcing-lahn.nc"
+config_src = './tests/hbv_config.toml'
+
+
+@pytest.fixture(scope="session")
+def model_config(tmp_path_factory):
+    tempdir = tmp_path_factory.mktemp("data")
+
+    urllib.request.urlretrieve(staticmaps_src, tempdir / 'staticmaps-lahn.nc')
+    urllib.request.urlretrieve(forcing_src, tempdir / 'forcing-lahn.nc')
+
+    config_dest = tempdir / 'hbv_config.toml'
+    shutil.copy(config_src, config_dest)
+
+    return str(config_dest)
+
+
+def test_bmi(model_config):
+    model = WflowBMI()
+    model.initialize(model_config)
