b3ae5b2b6191513d38fd99ab82bc3e65f38cbb72
Bart Schilperoort <b.schilperoort@gmail.com>
2022-11-04 09:03:16 +0100

    Remove unused pytest-printer

--- tests/test_wflow.py
+++ tests/test_wflow.py
@@ -1,23 +1,22 @@
-import pytest
-from pytest_print import printer
-from pywflow import WflowBMI
-import urllib
 import shutil
+import urllib
+import pytest
+from pywflow import WflowBMI
 
 
 staticmaps_src = "https://github.com/visr/wflow-artifacts/releases/download/v0.2.1/staticmaps-lahn.nc"
 forcing_src = "https://github.com/visr/wflow-artifacts/releases/download/v0.2.0/forcing-lahn.nc"
-config_src = './tests/hbv_config.toml'
+config_src = "./tests/hbv_config.toml"
 
 
 @pytest.fixture(scope="session")
 def model_config(tmp_path_factory):
     tempdir = tmp_path_factory.mktemp("data")
 
-    urllib.request.urlretrieve(staticmaps_src, tempdir / 'staticmaps-lahn.nc')
-    urllib.request.urlretrieve(forcing_src, tempdir / 'forcing-lahn.nc')
+    urllib.request.urlretrieve(staticmaps_src, tempdir / "staticmaps-lahn.nc")
+    urllib.request.urlretrieve(forcing_src, tempdir / "forcing-lahn.nc")
 
-    config_dest = tempdir / 'hbv_config.toml'
+    config_dest = tempdir / "hbv_config.toml"
     shutil.copy(config_src, config_dest)
 
     return str(config_dest)
